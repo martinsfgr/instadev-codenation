4674f4d0a758ef104ea41e829d85be75584dd676
Fagner <mauriciofagner2010@hotmail.com>
2020-05-28 13:10:17 -0300

    Adicionar sistema de curtidas

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Post.scss';
 
 const Post = ({ postInfo, userInfo }) => {
+  const [like, setLike] = useState(false);
 
   return (
     <article className="post" data-testid="post">
@@ -22,6 +23,18 @@ const Post = ({ postInfo, userInfo }) => {
       <figure className="post__figure">
         <img src={postInfo.imageUrl} alt="" />
       </figure>
+
+      <nav className="post__controls">
+        <button className="post__control" onClick={() => setLike(!like)}>
+          {like ? <i className="fas fa-heart" /> : <i className="far fa-heart" />}
+        </button>
+
+        <div className="post__status">
+          <div className="user">
+            <span>Curtido por {like ? postInfo.comments.length + 1 : postInfo.comments.length} pessoas.</span>
+          </div>
+        </div>
+      </nav>
 
     </article>
   );
